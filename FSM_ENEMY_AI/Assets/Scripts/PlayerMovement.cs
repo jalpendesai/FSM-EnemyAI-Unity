32c8ea100084267ffd8cce7335ab3851fabba7d0
jalpendesai <djalpen24@gmail.com>
2018-09-26 23:31:12 -0400

    Added Colliders, Player & Camera Movement

---
+++ FSM_ENEMY_AI/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public int speed = 1;
+
+    private float moveHorizontal;
+    private float moveVertical;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 20.0f;
+        moveVertical = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        transform.Rotate(0, moveHorizontal * speed, 0);
+        transform.Translate(0, 0, moveVertical * speed);
+    }
+}
